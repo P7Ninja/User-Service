656678c23a366067180cde1865e853d5ae67ffcc
Magnus <magnus@bonusfamilien.dk>
2023-11-20 10:28:55 +0100

    env changes

--- app/server.py
+++ app/server.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI
 
 from userservice import UserService
 from userservice.database import SQLUserDB
+from dotenv import dotenv_values
 
 import os
 
-cfg = os.environ
+if os.path.exists(".env"):
+    cfg = dotenv_values(".env")
+
+cfg["DB_CONN"] = os.environ.get("DB_CONN", cfg["DB_CONN"])
 
 app = FastAPI()
 
